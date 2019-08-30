70115371ba6541acc51be5efe2d6548005e59df4
poids <vhmorais@ucdavis.edu>
2019-08-29 17:21:02 -0700

    Fixed ubiquitous function!

--- tissue-expression.R
+++ tissue-expression.R
@@ -13,8 +13,8 @@ system=read.csv('Data/tissuesystem.csv', header = TRUE, sep = ",")
 colnames(system)=c("Tissue", "Organ", "System")
 
 #FUNCTION: If epitope is found in all systems it changes it so it just reads as ubiquitous instead
-ubiquitous = function(epitope_df) {
-  org_no=length(unique(epitope_df$Organ)) # of Organs in dataframe
+ubiquitous = function(epitope_df, org_no) {
+  #org_no=length(unique(epitope_df$Organ)) # of Organs in dataframe
   #epitope_df=add_row(epitope_df, Gene.name = "", Organ = 'Ubiquitous')#Inserts blank placeholder row for ubiquitous so it shows up on graph even if there are no hits
   for (epi in unique(epitope_df$Gene.name)) {
     if (sum(epitope_df$Gene.name==epi)==org_no) { #If gene is found in every tissue system,
@@ -30,7 +30,7 @@ tis_expr <- merge(tissue, system, by='Tissue') %>%
   select(Gene, Gene.name, Organ, System, Tissue, Cell.type, Level, Reliability)
 
 #Remove entries for pituitary gland and eye (Not enough data form Human Protein Atlas to be accurate)
-tis_expr=tis_expr[!(tis_expr$Organ=='Pituitary' | tis_expr$Organ=='Eye'),]
+tis_expr=tis_expr[!(tis_expr$Organ=='Pituitary' | tis_expr$Organ=='Eye' | tis_expr$Organ=='Foot' | tis_expr$Organ=='Thymus'),]
 
 #Count expression level
 tissue %>%
@@ -52,6 +52,12 @@ uniprot=read.csv('Data/protname_gene.csv', header = TRUE, sep = ",", stringsAsFa
 
 uniprot$Gene_Symbol<-toupper(uniprot$Gene_Symbol)#Make all proteins uppercase
 
+#Options to filter based on Organism and AutoImmune disease
+# disease_epitopes <- read.csv("~/Bioinformatics/Immunodietica/Data/disease_epitopes.csv", sep="")
+# uniprot=merge(uniprot, disease_epitopes, by.x = 'Epitope', by.y='description')
+# uniprot=uniprot[,1:4]
+# colnames(uniprot)=c('Epitope','UniProt_Name', 'Gene_Symbol', 'Disease')
+# uniprot=filter(uniprot, Disease=='ulcerative colitis,')
 
 #Iterates through entries that have multiple protein aliases and replaces it with the one usesd by Human Protein Atlas
 protein_aliases<-uniprot %>% filter(str_detect(Gene_Symbol, "//")) #Finds all rows wher e
@@ -98,7 +104,7 @@ test_organ<-tis_expr %>%
   filter(Reliability!='Uncertain') %>%
   select(Gene.name, Organ) %>%
   distinct() %>%
-  ubiquitous() %>% #Ubiquitous function
+  ubiquitous(., length(unique(tis_expr$Organ))) %>% #Ubiquitous function
   merge(., prot_freq, by='Gene.name') %>%
   count(Organ, wt=Freq)
   
@@ -118,19 +124,23 @@ mycol <- c("navy", "blue", "cyan", "lightcyan", "yellow", "red", "red4")
 #System
 ggplot(data=test_system, aes(x=System, y=n)) +
   geom_col(colour="black", aes(fill=n)) +
+  theme_classic() +
   theme(plot.title = element_text(hjust=0.5),
         axis.text.x = element_text(angle = 45,
                                    hjust = 1,
                                    vjust = 1)) +
   ggtitle('Epitope Tissue-Expression (System Level)') + ylab('Epitope Count') +
-  scale_fill_gradientn(colours = mycol)
+  scale_fill_gradient(high = "firebrick", low = "#ffce00", name = "# of Epitopes")
+  #scale_fill_gradientn(colours = mycol)
 
 #Organ
 ggplot(data=test_organ, aes(x=Organ, y=n)) +
   geom_col(colour="black",aes(fill=n)) +
+  theme_classic() +
   theme(plot.title = element_text(hjust=0.5),
         axis.text.x = element_text(angle = 45,
                                    hjust = 1,
                                    vjust = 1)) +
   ggtitle('Epitope Tissue-Expression (Organ Level)') + ylab('Epitope Count') +
-  scale_fill_gradientn(colours = mycol)
+  scale_fill_gradient(high = "firebrick", low = "#ffce00", name = "# of Epitopes")
+  #scale_fill_gradientn(colours = mycol)
